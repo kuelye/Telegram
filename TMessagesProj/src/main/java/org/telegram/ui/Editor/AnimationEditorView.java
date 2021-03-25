715ba9bf9ada9e0da2b2e9c80e4eeaf102ff8c82
kuelye <kuelye@gmail.com>
2021-03-25 22:10:32 +0300

    Animation Settings: pager

--- TMessagesProj/src/main/java/org/telegram/ui/Editor/AnimationEditorView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Editor/AnimationEditorView.java
@@ -1,39 +1,50 @@
 package org.telegram.ui.Editor;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Components.FilterTabsView;
 import org.telegram.ui.Components.LayoutHelper;
 
+import static androidx.viewpager.widget.ViewPager.SCROLL_STATE_DRAGGING;
+import static androidx.viewpager.widget.ViewPager.SCROLL_STATE_IDLE;
+
 public class AnimationEditorView extends LinearLayout {
 
     private final FilterTabsView tabsView;
+    private final ViewPager viewPager;
+    private final Adapter viewPagerAdapter;
 
     public AnimationEditorView(Context context) {
         super(context);
         setOrientation(LinearLayout.VERTICAL);
 
         // tabs
-        tabsView = new FilterTabsView(context) {
-
-        };
+        tabsView = new FilterTabsView(context);
         tabsView.setLayoutParams(LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 44));
         tabsView.setDelegate(new FilterTabsView.FilterTabsViewDelegate() {
             @Override
             public void onPageSelected(int page, boolean forward) {
-
+                viewPager.setCurrentItem(page);
             }
 
             @Override
             public void onPageScrolled(float progress) {
-
+                // stub
             }
 
             @Override
             public void onSamePageSelected() {
-
+                // TODO [CONTEST] scroll to top
             }
 
             @Override
@@ -53,12 +64,12 @@ public class AnimationEditorView extends LinearLayout {
 
             @Override
             public void onDeletePressed(int id) {
-
+                // stub
             }
 
             @Override
             public void onPageReorder(int fromId, int toId) {
-
+                // stub
             }
 
             @Override
@@ -73,9 +84,98 @@ public class AnimationEditorView extends LinearLayout {
         }
         tabsView.finishAddingTabs(true);
         tabsView.setBackgroundColor(Theme.getColor(Theme.key_actionBarDefault));
+
+        // pager
+        viewPager = new ViewPager(context);
+        viewPager.setLayoutParams(LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 0, 1, Gravity.NO_GRAVITY));
+        viewPagerAdapter = new Adapter(context);
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            private int previousState = -1;
+            private int previousPosition = -1;
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (positionOffset != 0) {
+                    if (previousPosition <= position) {
+                        position += 1;
+                    } else {
+                        positionOffset = 1 - positionOffset;
+                    }
+                    tabsView.selectTabWithId(position, positionOffset);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // stub
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (previousState != SCROLL_STATE_IDLE && (state == SCROLL_STATE_IDLE || state == SCROLL_STATE_DRAGGING)) {
+                    tabsView.selectTabWithId(viewPager.getCurrentItem(), 1);
+                    previousPosition = viewPager.getCurrentItem();
+                }
+                previousState = state;
+            }
+        });
+        addView(viewPager);
     }
 
-    public FilterTabsView getTabsView() {
-        return tabsView;
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        viewPagerAdapter.onDetached();
+    }
+
+    private static class Adapter extends PagerAdapter {
+
+        private final Context context;
+        private final View[] views;
+
+        Adapter(Context context) {
+            this.context = context;
+            views = new View[getCount()];
+        }
+
+        @Override
+        public int getCount() {
+            return AnimationType.values().length;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            View view = views[position];
+            if (view == null) {
+                if (position == 0) {
+                    view = new BackgroundAnimationEditorView(context);
+                } else {
+                    view = new DefaultAnimationEditorView(context, AnimationType.values()[position]);
+                }
+                view.setBackgroundColor((int) (Math.random() * 0x1000000) + 0xFF000000);
+                views[position] = view;
+            }
+            container.addView(view, 0);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        public void onDetached() {
+            for (int i = 0, l = views.length; i < l; i++) {
+                views[i] = null;
+            }
+        }
     }
 }
